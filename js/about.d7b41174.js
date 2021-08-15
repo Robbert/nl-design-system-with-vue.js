b62c6238196076e619726f92722e1967baa5b0e9
Robbert <mail@robbertbroersma.nl>
2021-08-15 15:24:27 +0000

    Deploying to gh-pages from @ Robbert/nl-design-system-with-vue.js@07e23b00e13eccaf59888041ce6f79c0e4fe8b64 🚀

---
+++ js/about.d7b41174.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,e,n){"use strict";n.r(e);var a=n("7a23"),c={class:"about utrecht-html"},u=Object(a["e"])("h1",null,"This is an about page",-1),o=[u];function s(t,e){return Object(a["q"])(),Object(a["d"])("div",c,o)}const b={};b.render=s;e["default"]=b}}]);
+//# sourceMappingURL=about.d7b41174.js.map
